46143ab233d059fdca9334818ca1e061e5c77f38
Kevin Nhan <kevinnhan.2.1.1@gmail.com>
2025-11-05 13:30:04 -0500

    Initial Commit

---
+++ LockedInFunctionality/LockedInFunctionalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedInFunctionalityApp.swift
+//  LockedInFunctionality
+//
+//  Created by Kevin Nhan on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct LockedInFunctionalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
