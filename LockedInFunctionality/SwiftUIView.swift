832abb1f15444cf05b929f269118057d064a63c0
Kevin Nhan <kevinnhan.2.1.1@gmail.com>
2025-11-07 15:08:32 -0500

    hi

---
+++ LockedInFunctionality/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  LockedInFunctionality
+//
+//  Created by Kevin Nhan on 2025-11-07.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
